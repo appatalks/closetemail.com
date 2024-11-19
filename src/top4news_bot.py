d94bd3cb7f0c1a52da1fc155ef7fcbf395672b99
Steven B. <4163156+appatalks@users.noreply.github.com>
2024-11-19 11:53:27 -0600

    Update top4news_bot.py

--- src/top4news_bot.py
+++ src/top4news_bot.py
@@ -2,7 +2,9 @@ import os
 import requests
 import subprocess
 import string
+import re
 from datetime import datetime, timezone
+from typing import List, Dict
 
 # Function to log in to Bluesky
 def bsky_login_session(pds_url: str, handle: str, password: str):
@@ -15,12 +17,33 @@ def bsky_login_session(pds_url: str, handle: str, password: str):
 
 # Function to create a Bluesky post
 def create_bsky_post(session, pds_url, post_content, embed=None):
+    # Extract URLs from the post content
+    link_spans = parse_urls(post_content)
+    
     now = datetime.now(timezone.utc).isoformat().replace("+00:00", "Z")
     post = {
         "$type": "app.bsky.feed.post",
         "text": post_content,
         "createdAt": now,
     }
+
+    # Include the links in the post using the extracted link spans
+    if link_spans:
+        post["facets"] = [
+            {
+                "features": [
+                    {
+                        "$type": "app.bsky.richtext.facet#link",
+                        "uri": span["url"]
+                    }
+                ],
+                "index": {
+                    "start": span["start"],
+                    "end": span["end"]
+                }
+            } for span in link_spans
+        ]    
+    
     if embed:
         post["embed"] = embed
     
@@ -105,6 +128,21 @@ def get_date_with_suffix():
         suffix = {1: "st", 2: "nd", 3: "rd"}.get(day % 10, "th")
     return today.strftime(f"%A, %B {day}{suffix}, %Y")
 
+# Function to parse URLs from text and return a list of spans
+def parse_urls(text: str) -> List[Dict]:
+    spans = []
+    # Partial/naive URL regex based on: https://stackoverflow.com/a/3809435
+    # Tweaked to disallow some trailing punctuation
+    url_regex = rb"[$|\W](https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*[-a-zA-Z0-9@%_\+~#//=])?)"
+    text_bytes = text.encode("UTF-8")
+    for m in re.finditer(url_regex, text_bytes):
+        spans.append({
+            "start": m.start(1),
+            "end": m.end(1),
+            "url": m.group(1).decode("UTF-8"),
+        })
+    return spans
+
 def main():
     # Bluesky setup
     pds_url = "https://bsky.social"
@@ -125,7 +163,7 @@ def main():
     # Format the date and additional text
     formatted_date = get_date_with_suffix()
     additional_text = f"4th at source: https://ground.news/"
-    post_header = f"Top Headlines for {formatted_date}:\n"
+    post_header = f"{formatted_date}:\n"
     
     # Reduce content to 300 characters
     trimmed_headlines = reduce_to_300_chars(top4_news, post_header + additional_text)
